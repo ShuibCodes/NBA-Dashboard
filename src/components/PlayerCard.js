01db16e6b3c6e5d34cc6628c30f56469bbf7c2ac
Shuib Abdillahi <71759511+ShuibCodes@users.noreply.github.com>
2022-03-08 11:23:07 +0000

    fixed typo

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -4,7 +4,8 @@ function PlayerCard(props) {
 	return (
 		<Card>
 			<div className="p-4">
-s00 text-center py-8">{props.name}</h1>
+
+				<h1 className="text-lg leading-6 font-medium text-gray-900 text-center py-8">{props.name}</h1>
 
 				<h1 className="text-lg leading-6 font-medium text-gray-900">{props.playerTeam}</h1>
 				<h6 className="text-lg leading-6 font-medium text-gray-900">Games Played: <span
@@ -15,15 +16,15 @@ s00 text-center py-8">{props.name}</h1>
 
 				<dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
 					<div className="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
-						<dt className="text-sm font-medium text-gray-500 truncate">Points:</dt>
+						<dt className="text-sm font-medium text-gray-500 truncate">Points</dt>
 						<dd className="mt-1 text-3xl font-semibold text-gray-900">{props.playerPoints}</dd>
 					</div>
 					<div className="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
-						<dt className="text-sm font-medium text-gray-500 truncate">Assists:</dt>
+						<dt className="text-sm font-medium text-gray-500 truncate">Assists</dt>
 						<dd className="mt-1 text-3xl font-semibold text-gray-900">{props.playerAssist}</dd>
 					</div>
 					<div className="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
-						<dt className="text-sm font-medium text-gray-500 truncate">Rebounds:</dt>
+						<dt className="text-sm font-medium text-gray-500 truncate">Rebounds</dt>
 						<dd className="mt-1 text-3xl font-semibold text-gray-900">{props.playerRebounds}</dd>
 					</div>
 				</dl>
